645cc6604342ce92b3689a44d36bfdbb93a56bb9
Julius Rückin <julius.rueckin@rats-os.de>
2015-01-21 18:46:04 +0100

    added opportunity to preselect table cells with mouse click and made them visible by new background colors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,19 @@
 
 $(document).ready(function(){
 	$(".availability_plan .free_cell_style_1, .availability_plan .free_cell_style_2, .availability_plan .booked_cell_style").click(function(){
-		alert($(this).html() + " - " + $(this).attr("data-court") + " - " + $(this).attr("data-begin") + " Uhr bis " + $(this).attr("data-end") + " Uhr");
+		//alert($(this).html() + " - " + $(this).attr("data-court") + " - " + $(this).attr("data-begin") + " Uhr bis " + $(this).attr("data-end") + " Uhr");
+		if($(this).attr("data-bookable") == "1"){
+			if($(this).attr("data-preselected") == "0"){
+				$(this).css("background","#837E7C");
+				$(this).attr("data-preselected","1");
+			}
+			else{
+				$(this).css("background","blue");
+				$(this).attr("data-preselected","0");
+			}
+		}
+		else{
+			$(this).css("background","red");
+		}
 	});
 });
