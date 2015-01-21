240398000c28c9471210f90db8a6d8d19698bc40
Julius Rückin <julius.rueckin@rats-os.de>
2015-01-21 18:20:42 +0100

    added information data-attributes for assignment plan table cells, accessible by clicking on table cell

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,5 +4,7 @@
 //= require_tree .
 
 $(document).ready(function(){
-	
+	$(".availability_plan .free_cell_style_1, .availability_plan .free_cell_style_2, .availability_plan .booked_cell_style").click(function(){
+		alert($(this).html() + " - " + $(this).attr("data-court") + " - " + $(this).attr("data-begin") + " Uhr bis " + $(this).attr("data-end") + " Uhr");
+	});
 });
